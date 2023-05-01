7907368cd9a103ae885fb311d8d66448f38dc99a
Muhammad-Sameed-Ansari <sameedanxari@gmail.com>
2023-05-01 13:04:07 +0500

    Initial Commit

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Sameed Ansari on 28/04/2023.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
